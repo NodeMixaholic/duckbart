2e7af95f2fa8171ab8f9c6c82b15d8f48160fb82
NodeMixaholic <sam@sllord.info>
2023-02-10 11:15:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,10 @@ while True:
           raise Exception("Not understand!")
         print("<USER>:" + ans.replace("<BOT>:<USER>", "").replace("\\r", ""))
     except:
+      s = duckduckgo.get_zci(q)
       o = query({
-      "inputs": "<USER>: " + q + " <BOT>:",
-      }, "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B")
-      ans = o[0]['generated_text'].split("<USER>:")[1]
-      if ('don\'t understand' in ans or 'don\'t know' in ans or 'try' in ans or 'not sure' in ans):
-        raise Exception("Not understand!")
-      print("<USER>:" + ans.replace("<BOT>:<USER>", "").replace("\\r", ""))
+      "inputs": s,
+      }, "https://api-inference.huggingface.co/models/google/pegasus-xsum")
+      print("<USER>: " + q + " <BOT>:" + o[0]['summary_text'])
   except:
     print("Result not found.")
