1509e98de0a57410d3cb6d231ef71ebeea737b39
NodeMixaholic <sam@sllord.info>
2023-02-10 10:45:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ while True:
   q = input("prompt:")
   try:
     try:
-      o = query({
-      "inputs": "<USER>: " + q + " <BOT>:",
-      }, "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B")
-      ans = o[0]['generated_text'].split("<USER>:")[1]
-      if ('don\'t understand' in ans or 'don\'t know' in ans or 'try' in ans or 'not sure' in ans):
-        raise Exception
-      print("<USER>:" + ans.replace("<BOT>:<USER>", "").replace("\\r", ""))
-    except:
       s = duckduckgo.get_zci(q)
       o = query({
       "inputs": s,
       }, "https://api-inference.huggingface.co/models/google/pegasus-xsum")
       print("<USER>: " + q + " <BOT>:" + o[0]['summary_text'])
+    except:
+      o = query({
+      "inputs": "<USER>: " + q + " <BOT>:",
+      }, "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B")
+      ans = o[0]['generated_text'].split("<USER>:")[1]
+      if ('don\'t understand' in ans or 'don\'t know' in ans or 'try' in ans or 'not sure' in ans):
+        raise Exception("Not understand!")
+      print("<USER>:" + ans.replace("<BOT>:<USER>", "").replace("\\r", ""))
   except:
     print("Result not found.")
