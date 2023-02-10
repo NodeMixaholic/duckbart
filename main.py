2d20d0de3e0241b1e160f82621b6d096742e3dc7
NodeMixaholic <sam@sllord.info>
2023-02-10 11:13:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ while True:
   q = input("prompt:")
   try:
     try:
-      s = duckduckgo.get_zci(q)
-      o = query({
-      "inputs": s,
-      }, "https://api-inference.huggingface.co/models/google/pegasus-xsum")
-      print("<USER>: " + q + " <BOT>:" + o[0]['summary_text'])
+      if q.startswith("Answer this:") or q.startswith("Answer mode:") or q.startswith("Ans:") or q.startswith("Answer:"):
+        s = duckduckgo.get_zci(q)
+        o = query({
+        "inputs": s,
+        }, "https://api-inference.huggingface.co/models/google/pegasus-xsum")
+        print("<USER>: " + q + " <BOT>:" + o[0]['summary_text'])
+      else:
+        o = query({
+        "inputs": "<USER>: " + q + " <BOT>:",
+        }, "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B")
+        ans = o[0]['generated_text'].split("<USER>:")[1]
+        if ('don\'t understand' in ans or 'don\'t know' in ans or 'try' in ans or 'not sure' in ans):
+          raise Exception("Not understand!")
+        print("<USER>:" + ans.replace("<BOT>:<USER>", "").replace("\\r", ""))
     except:
       o = query({
       "inputs": "<USER>: " + q + " <BOT>:",
