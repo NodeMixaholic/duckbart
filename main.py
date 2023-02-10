1e05756f657c62d7313f35ff9e29cd7c1161db13
NodeMixaholic <sam@sllord.info>
2023-02-09 21:00:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ while True:
   try:
     try:
       o = query({
-      "inputs": q,
-      }, "https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium")
-      ans = o['generated_text']
+      "inputs": "<USER>: " + q + " <BOT>:",
+      }, "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B")
+      ans = o[0]['generated_text'].split("<USER>:")[1]
       if ('don\'t understand' in ans or 'don\'t know' in ans or 'try' in ans or 'not sure' in ans):
         raise Exception
-      print(ans)
+      print("<USER>: " + ans.replace("<BOT>:<USER>", "").replace("\\r", ""))
     except:
       s = duckduckgo.get_zci(q)
       o = query({
       "inputs": s,
       }, "https://api-inference.huggingface.co/models/google/pegasus-xsum")
-      print(o[0]['summary_text'])
+      print("<USER>: " + q + " <BOT>:" + o[0]['summary_text'])
   except:
     print("Result not found.")
